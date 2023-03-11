63c87602bffb5628d7b0917fba74f12971f70f5b
Mansi-1256 <65182356+Mansi-1256@users.noreply.github.com>
2023-03-11 12:05:02 +0530

    first commit

---
+++ controller/UserController.js
@@ -0,0 +1,71 @@
+import User from "../model/user.js";
+
+
+export const signup = async (req, res) => {
+    const { email, firstName, lastName, phone } = req.body
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(400).json({ message: "User already exist" });
+        const result = await User.create({ email, name: `${firstName}${lastName}`, phone })
+        res.status(200).json({ result, message: "Register sucessfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}
+export const signin = async (req, res) => {
+    const { email, firstName, lastName, phone } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser)
+            return res.status(404).json({ message: "User doesn't exist" });
+        res.status(200).json({ result: existingUser, message: "Sign in sucesfully" })
+
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
+export const getuser = async (req, res) => {
+    const { email } = req.body
+    try {
+        const getuser = await User.find()
+        return res.status(200).json({ getuser })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getuserbyId = async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const getuser = await User.findById(req.params._id)
+        return res.status(200).json({ getuser })
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const updateuser = async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const updateuser = await User.findByIdAndUpdate({ _id }, { $set: req.body }, { new: true })
+        return res.status(200).json({ updateuser, message: "updated user" })
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+export const deleteuser = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params._id)
+        res.status(500).json({ message: "user deleted sucessfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
+
+
+
+
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import { signup, signin, updateuser, deleteuser, getuser, getuserbyId } from "../controller/UserController.js";
+const router = express.Router();
+
+
+router.post('/signup', signup)
+router.post('/signin', signin)
+router.get('/getuser', getuser)
+router.get('/getuser/:_id', getuserbyId)
+router.patch('/updateuser/:_id', updateuser)
+router.delete('/deleteuser/:_id', deleteuser)
+
+
+export default router;
